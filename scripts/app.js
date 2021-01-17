44585a09b73e616dc2b15e52f01c4300e8e61887
okawahiroto <okawa.hiroto@gmail.com>
2021-01-18 03:37:02 +0900

    Firebase Auth〜 チュートリアル、メール認証途中

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCj1zsGo6RsTWKU1OQgCKwSzSC5ST7L3_Q",
+  authDomain: "fbstudy01.firebaseapp.com",
+  databaseURL: "https://fbstudy01.firebaseio.com",
+  projectId: "fbstudy01",
+  storageBucket: "fbstudy01.appspot.com",
+  messagingSenderId: "753037162385",
+  appId: "1:753037162385:web:68f76e6272a7e44e55cc30",
+  measurementId: "G-KXGZY2D1TQ"
+};
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('btnLogin');
+const btnSignup = document.getElementById('btnSignUp');
+const btnLogout = document.getElementById('btnLogout');
+
